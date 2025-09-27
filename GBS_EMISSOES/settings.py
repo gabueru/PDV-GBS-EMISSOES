01847c621d3e94491b28f1f0abc71ba8494830df
Gabas <gabrielbrandaobrasil2014@gmail.com>
2025-09-27 12:25:05 -0300

    Update settings.py

--- GBS_EMISSOES/settings.py
+++ GBS_EMISSOES/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles')  # Para produção
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Para produção
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
